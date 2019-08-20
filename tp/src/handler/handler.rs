ab11ae075a8fe97e65235679c9fb836e9fb01d6e
LudeeD <lfssilva@ua.pt>
2019-08-20 17:31:33 +0100

    payload is expected in cbor

--- tp/src/handler/handler.rs
+++ tp/src/handler/handler.rs
@@ -2,11 +2,12 @@ use sawtooth_sdk::messages::processor::TpProcessRequest;
 use sawtooth_sdk::processor::handler::ApplyError; use sawtooth_sdk::processor::handler::TransactionContext;
 use sawtooth_sdk::processor::handler::TransactionHandler;
 
-use crate::handler::payload::Action;
-use crate::handler::payload::{PayloadBuilder, PayloadCreateVote, PayloadVote, PayloadCloseVote};
 use crate::handler::state::get_sw_prefix;
 use crate::handler::state::SwState;
 use crate::handler::vote::Vote;
+
+use std::collections::BTreeMap;
+use serde_cbor::from_slice;
 
 pub struct SwTransactionHandler {
     family_name: String,
@@ -16,17 +17,13 @@ pub struct SwTransactionHandler {
 
 //Transactions in dignitas
 trait SwTransactions {
-    fn create_vote(&self, state: &mut SwState,info : PayloadCreateVote) 
+    fn create_vote(&self, state: &mut SwState,info : BTreeMap<String,String>)
         -> Result<(), ApplyError>;
 
-    fn vote(
-        &self,
-        state: &mut SwState,
-        customer_pubkey: &str,
-        info: PayloadVote,
-        ) -> Result<(), ApplyError>;
+    fn vote( &self, state: &mut SwState, customer_pubkey: &str, info: BTreeMap<String,String>,)
+        -> Result<(), ApplyError>;
 
-    fn close_vote(&self, state: &mut SwState, info: PayloadCloseVote)
+    fn close_vote(&self, state: &mut SwState, info: BTreeMap<String,String>)
         -> Result<(), ApplyError>;
 }
 
@@ -70,69 +67,86 @@ impl TransactionHandler for SwTransactionHandler {
             }
         };
 
-        let payload_builder = PayloadBuilder::new(request.get_payload());
-
-        let payload_builder = match payload_builder {
-            Err(e) => return Err(e),
-            Ok(payload) => payload,
-        };
+        let payload: BTreeMap<String, String>
+            = from_slice(request.get_payload()).expect("Failed Unpacking Payload");
 
         let mut state = SwState::new(context);
 
-        let _payload = match payload_builder.get_action(){
-            Action::CreateVote =>{
-                self.create_vote(&mut state, payload_builder.create_vote_payload())
-            },
-            Action::Vote =>{
-                let vote_payload = payload_builder.vote_payload();
-                let vote_id = vote_payload.vote_id.clone();
-                let vote_value = vote_payload.value.clone();
-                self.vote(&mut state, customer_pubkey, vote_payload)?;
+        match payload.get("action") {
+            Some(action) => {
+                match action.as_ref() {
+                    "create" =>{
+                        self.create_vote(&mut state, payload)
+                    },
+                    "vote" => {
+                        let vote_id = payload.get("voteID").unwrap();
+                        let vote_value = payload.get("value").unwrap();
+                        self.vote(&mut state, customer_pubkey, payload.clone())?;
 
-                context.add_event(
-                    "dignitas/create".to_string(),
-                    vec![   ("vote_id".to_string(), vote_id),
+                        context.add_event(
+                            "dignitas/create".to_string(),
+                            vec![   ("vote_id".to_string(), vote_id.to_string()),
                             ("voter".to_string(),   customer_pubkey.to_string()),
                             ("value".to_string(),   vote_value.to_string()),
-                    ],
-                    vec![].as_slice()).expect("Something Went wrong sending the Vote Event");
+                            ],
+                            vec![].as_slice()).expect("Something Went wrong sending the Vote Event");
 
+                        Ok(())
+                    },
+                    "close" => {
+                        self.close_vote(&mut state, payload)
+                    },
+                    _ => {
+                        Err(ApplyError::InternalError(String::from("No Action in Payload")))
+                    }
+                }.expect("Damn, pretty bad error");
                 Ok(())
-
             },
-            Action::CloseVote =>{
-                self.close_vote(&mut state, payload_builder.close_vote_payload())
+            None =>{
+                Err(ApplyError::InternalError(String::from("No Action in Payload")))
             }
-        };
-        info!("Apply Function Exited");
-
-        Ok(())
+        }
     }
 }
 
 impl SwTransactions for SwTransactionHandler {
 
-    fn create_vote(&self, state: &mut SwState, info: PayloadCreateVote)
-        -> Result<(), ApplyError> {
+    fn create_vote(&self, state: &mut SwState, info: BTreeMap<String,String>) -> Result<(), ApplyError> {
 
-            info!("Create Vote Called");
+            info!("Creating New Vote");
 
-            let vote = Vote::new( info.lat, info.lng, info.direction,
-                                  &info.title, &info.info, info.timestamp);
+            let title   = info.get("title").expect("Bad Payload");
+            let details = info.get("info").expect("Bad Payload");
+            let lat: f64= info.get("lat").expect("Bad Payload")
+                                .parse().expect("Bad Payload");
+            let lng: f64= info.get("lng").expect("Bad Payload")
+                                .parse().expect("Bad Payload");
+            let dir: f64= info.get("dir").expect("Bad Payload")
+                                .parse().expect("Bad Payload");
+            let time:u64= info.get("timestamp").expect("Bad Payload")
+                                .parse().expect("Bad Payload");
 
-            state.set_vote(vote).expect("Something Went Wrong");
+            let vote = Vote::new(lat,lng,dir,title,details,time);
 
-            Ok(())
+            state.set_vote(vote)
         }
 
     fn vote(
         &self,
         state: &mut SwState,
         customer_pubkey: &str,
-        info: PayloadVote,
+        info: BTreeMap<String,String>,
     ) -> Result<(), ApplyError> {
 
-        info!("Vote Called");
+        info!("Voting");
+
+        let value :i64 = info.get("value")
+            .expect("Bad Payload")
+            .parse()
+            .expect("Bad Payload - Failed Parsing");
+
+        let vote_id :String = info.get("voteID")
+            .expect("Bad Payload").to_string();
 
         let current_balance: i64 = match state.get_balance(customer_pubkey) {
             Ok(Some(v)) => v,
@@ -146,19 +160,15 @@ impl SwTransactions for SwTransactionHandler {
             Err(err) => return Err(err),
         };
 
-        info!("Current Balance fetched");
-
-        let abs_value = info.value.abs();
+        let abs_value = value.abs();
 
         if abs_value > current_balance {
-            return Err(ApplyError::InvalidTransaction(String::from(
-                        "You Don't have the credits for it",
-                        )));
+            return Err(ApplyError::InvalidTransaction(String::from("You Don't have the credits for it",)));
         }else{
             state.set_balance(customer_pubkey, current_balance-abs_value).expect("Something Went Wrong");
         };
 
-        let mut vote = match state.get_vote(info.vote_id) {
+        let mut vote = match state.get_vote(vote_id) {
             Ok(Some(v)) => v,
             Ok(None) => return Err(ApplyError::InvalidTransaction(String::from(
                         "Deal with this later",
@@ -167,20 +177,23 @@ impl SwTransactions for SwTransactionHandler {
         };
         info!("Vote state fetched");
 
-        // missing parameters 
-        if info.value.is_positive() {
+        // maybe refactor to had more fields to the payload instead of playing with positive and
+        // negatives
+
+        if value.is_positive() {
             vote.agree_more(abs_value as i64).expect("Something Went Wrong");
         }else{
             vote.disagree_more(abs_value as i64).expect("Something Went Wrong");
         };
 
         state.set_vote( vote ).expect("Something Went Wrong");
-        info!("Vote state updated");
+        info!("Vote State updated");
         Ok(())
     }
 
-    fn close_vote(&self, state: &mut SwState, info: PayloadCloseVote)
+    fn close_vote(&self, state: &mut SwState, info: BTreeMap<String,String>)
         -> Result<(), ApplyError> {
+            info!("Close Vote");
             Ok(())
         }
 }
--- tp/src/handler/mod.rs
+++ tp/src/handler/mod.rs
@@ -1,4 +1,3 @@
 pub mod handler;
-mod payload;
 mod state;
 mod vote;
